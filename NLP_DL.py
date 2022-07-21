6ebe3f19d0b3c5c2fbbc534363dd8be8ca222e04
서효정 <105341847+hyojjamppong@users.noreply.github.com>
2022-07-22 08:56:31 +0900

    Update and rename NLP.py to NLP_DL.py

--- NLP_DL.py
+++ NLP_DL.py
@@ -11,3 +11,7 @@ tokenizer.fit_on_texts(corpus)
 tokenizer.word_index  # 인덱스 해당 단어 확인
 tokenizer.word_counts.items() # 단어별 빈도수 확인
 pd.DataFrame(tokenizer.word_counts.items()).set_index(0).sort_values(by=1).T
+corpus_sentences = tokenizer.text_to_sequences(corpus)
+
+from tensorflow.keras.preprocessing.sequence import pad_sequence
+pad_sequence(corpus_sentences, max_len=10, padding="pre", truncating="pre")
